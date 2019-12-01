a2b8d833f322383cfa1988b25d0461b3934a3743
Count Count <countvoncount123456@gmail.com>
2019-12-01 14:50:58 +0100

    restart greeting on Dec 2 because of messed up control group

--- greetbot.py
+++ greetbot.py
@@ -298,7 +298,7 @@ class GreetController:
                 elif user.getUserTalkPage().exists():
                     # User talk page exists, will thus not be greeted.
                     pass
-                elif inProduction and not timezone.localize(datetime(2019, 12, 1, 0, 0)) < logevent.timestamp().replace(
+                elif inProduction and not timezone.localize(datetime(2019, 12, 2, 0, 0)) < logevent.timestamp().replace(
                     tzinfo=pytz.utc
                 ).astimezone(timezone) < timezone.localize(datetime(2020, 1, 26, 0, 0)):
                     # only greet users registered in eight week test period
