900daca4b8ce528de03e99b5d70ae1bbbf5dae72
Count Count <countvoncount123456@gmail.com>
2019-12-13 20:01:05 +0100

    scheduled job for collecting stats

--- tools/push-to-toolforge.sh
+++ tools/push-to-toolforge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pscp ../{greetbot.py,stats.py,Pipfile,Pipfile.lock} exec-bot.sh update-stats.sh greetbot-deployment.yaml countcount@login.tools.wmflabs.org:/data/project/dewikigreetbot/
+pscp ../{greetbot.py,stats.py,Pipfile,Pipfile.lock} exec-bot.sh update-stats.sh *.yaml countcount@login.tools.wmflabs.org:/data/project/dewikigreetbot/
 plink countcount@login.tools.wmflabs.org "chmod 755 /data/project/dewikigreetbot/exec-bot.sh"
 plink countcount@login.tools.wmflabs.org "chmod 755 /data/project/dewikigreetbot/update-stats.sh"
 plink countcount@login.tools.wmflabs.org become dewikigreetbot kubectl delete pods --all
