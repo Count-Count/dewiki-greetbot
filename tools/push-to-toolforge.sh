568a8ae1ae7aae787966e6f14b18ee6a3d004305
Nintendofan885 <66736551+Nintendofan885@users.noreply.github.com>
2020-08-03 15:06:13 +0100

    Update service name for SSH
    See https://wikitech.wikimedia.org/wiki/News/Toolforge.org#New_service_names_for_accessing_Toolforge_SSH_bastions for more information

--- tools/push-to-toolforge.sh
+++ tools/push-to-toolforge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pscp ../{greetbot.py,stats.py,sseclient.py,Pipfile,Pipfile.lock} exec-bot.sh update-stats.sh *.yaml countcount@login.tools.wmflabs.org:/data/project/dewikigreetbot/
-plink countcount@login.tools.wmflabs.org "chmod 755 /data/project/dewikigreetbot/exec-bot.sh"
-plink countcount@login.tools.wmflabs.org "chmod 755 /data/project/dewikigreetbot/update-stats.sh"
-plink countcount@login.tools.wmflabs.org become dewikigreetbot kubectl delete pods --all
+plink countcount@login.toolforge.org "chmod 755 /data/project/dewikigreetbot/exec-bot.sh"
+plink countcount@login.toolforge.org "chmod 755 /data/project/dewikigreetbot/update-stats.sh"
+plink countcount@login.toolforge.org become dewikigreetbot kubectl delete pods --all
